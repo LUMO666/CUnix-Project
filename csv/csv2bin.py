12d5ec40cee541229be40d6974f6cc5c64d4f49a
LUMO666 <liuwl20@mails.tsinghua.edu.cn>
2020-11-06 14:19:25 +0800

    csv2bin

--- csv/csv2bin.py
+++ csv/csv2bin.py
@@ -9,8 +9,30 @@ def readcsv(filename):
     
     return chart
 
-def writebin(bin):
+def writebin(data,bin):
+    with open(bin,'wb') as binFile:
+        n = '\n'
+        for line in data:
+            for item in line:
+                for char in (item+'\t'):
+                    binFile.write((char).encode(encoding='utf-8'))
+            binFile.write(n.encode(encoding='utf-8'))
+
+def readbin(bin):
+    with open(bin,'rb') as binFile:
+        str =''
+        p = None
+        while True:
+            p = binFile.read(1).decode(encoding='utf-8')
+            str += p
+            if p == '':
+                break
+        print(str)
 
 if __name__ == '__main__':
-    chart = readcsv("chart.csv")
+    binpath = 'bchart'
+    csvpath = 'chart.csv'
+    chart = readcsv(csvpath)
     print(chart)
+    writebin(chart,binpath)
+    readbin(binpath)
